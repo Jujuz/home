9eca62f309bafce3375e6c19a4dca83843cbb9f6
Jujuz <jessesaddress@gmail.com>
2018-09-04 15:44:26 +1000

    Basic browser python script

--- browserJacker.py
+++ browserJacker.py
@@ -1,1 +1,11 @@
-testtt
+import webbrowser
+import time
+import random
+
+while True:
+    sites = random.choice(['cnn.com','puppies.com','youtube.com'])
+    visit = "http://{}".format(sites)
+    webbrowser.open(visit)
+    seconds = random.randrange(5,100)
+    seconds = random.randrange(5, 20)
+    time.sleep(seconds)
